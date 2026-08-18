3d5a449555c9c162817cb66972c839740d6852b2
ArgonautCaptain <jbsoccer25@hotmail.com>
2026-08-17 22:35:51 -0700

    Added symbol of the warspear

--- MRT/RaidCheck.lua
+++ MRT/RaidCheck.lua
@@ -246,6 +246,7 @@ module.db.coaBuffs = {
 			[572390] = true, -- Greater Devotion of Dawn — Sun Cleric - Class Buff
 			[680303] = true, -- Greater Inquisitor's Edict — Witch Hunter - Class Buff
 			[712458] = true, -- Greater Power Wuju — Witch Doctor - Class Buff
+			[804730] = true, -- Symbol of the Warspear — Barbarian - Class Buff
 		},
 		-- No Spell ID in spreadsheet:
 		--   Barbarian - Class Buff — Removed?
